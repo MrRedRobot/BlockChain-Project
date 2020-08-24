5a10ba434896ef7727b7f2eed5a9a0df31e79b85
David_Orjuela <42826331+MrRedRobot@users.noreply.github.com>
2020-08-24 16:55:27 -0500

    Upload Inicial

---
+++ contracts/TodoList.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("Arriendo Oficina 101"); 
+    createTask("Arriendo Oficina 102");
+    createTask("Arriendo Oficina 201");
+    createTask("Arriendo Oficina 202");
+    createTask("Arriendo Oficina 301");
+    createTask("Arriendo Oficina 302");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+}
